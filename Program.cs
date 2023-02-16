c53fa708222696497c1824bbdfa67a981612af01
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-16 19:52:51 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace UserRegistration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To User Registration Program");
+            Console.WriteLine("Choose a VAlid Option From Below\n" +
+                "1]: For User Name validation");
+                
+            int Option = Convert.ToInt32(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    UserName.FirstName();
+                    break;
+                   
+                default:
+                    Console.WriteLine("Choose the valid option");
+                    break;
+            }
+        }
+    }
+}

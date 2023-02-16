ef7925baef2ce870df49912710110cc5d3f42406
[mahapatrasonali] <[sonangr2000@gmail.com]>
2023-02-16 19:56:22 +0530

    First Name Validation

--- Program.cs
+++ Program.cs
@@ -5,20 +5,11 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To User Registration Program");
-            Console.WriteLine("Choose a VAlid Option From Below\n" +
-                "1]: For User Name validation");
-                
-            int Option = Convert.ToInt32(Console.ReadLine());
-            switch (Option)
-            {
-                case 1:
-                    UserName.FirstName();
-                    break;
-                   
-                default:
-                    Console.WriteLine("Choose the valid option");
-                    break;
-            }
+            UserName.FirstName();
+
+
+
+
         }
     }
 }
